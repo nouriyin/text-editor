426cbaad057732711717802c062b0f54a519fe21
nouriyin <courtneyy.noreen@gmail.com>
2023-06-13 00:15:41 -0400

    commit and add code to index and packages, css

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,41 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+  export const putDb = async (content) => {
+    const connectDb = await openDB("jate", 1);
+  
+    const transact = connectDb.transaction("jate", "readwrite");
+  
+    const store = transact.objectStore("jate");
+  
+    const req = store.put({ id: 1, value: content });
+  
+    const res = await req;
+    console.log("Data saved to db", res);
+  };
+  
+  export const getDb = async () => {
+    const connectDb = await openDB("jate", 1);
+  
+    const transact = connectDb.transaction("jate", "readonly");
+  
+    const store = transact.objectStore("jate");
+  
+    const req = store.getAll();
+  
+    const res = await req;
+    console.log("result.value", res);
+    return res?.value;
+  };
+  initdb();
---
+++ Develop/client/src/js/install.js
@@ -0,0 +1,26 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    window.deferredPrompt = event;
+  
+    butInstall.classList.toggle("hidden", false);
+  });
+  
+  butInstall.addEventListener("click", async () => {
+    const promptEv = window.deferredPrompt;
+  
+    // guard clause
+    if (!promptEv) return;
+  
+    promptEv.prompt();
+  
+    window.deferredPrompt = null;
+  
+    butInstall.classList.toggle("hidden", true);
+  });
+  
+  window.addEventListener("appinstalled", (event) => {
+    window.deferredPrompt = null;
+  });
